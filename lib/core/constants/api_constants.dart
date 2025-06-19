363cab81129059f19701220cd99bb6494db99390
Arya Jagadditha <czajr13@gmail.com>
2025-06-20 02:40:52 +0700

    feat: intial feat

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,8 @@
 class ApiConstants {
   // Base Configuration
   static const String baseUrl =
-      'http://10.0.2.2:8000/api/v1'; // Android emulator
+      // 'http://10.0.2.2:8000/api/v1'; // Android emulator
+      'http://192.168.0.34:8000/api/v1'; // Android emulator
   // static const String baseUrl = 'http://localhost:8000/api/v1'; // iOS simulator
   // static const String baseUrl = 'https://your-domain.com/api/v1'; // Production
 
